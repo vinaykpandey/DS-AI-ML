73f29bbcc889e0789e53949bf4664bde6c13bb88
vinay pandey <vinay.pandey12@gmail.com>
2021-01-24 21:33:23 +0530

    numpy functions

--- num_py/6_properties_and_func.py
+++ num_py/6_properties_and_func.py
@@ -1,2 +1,42 @@
 import numpy as np
 print(np)
+
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype="float32")
+
+print(a.ndim)
+
+print(a.size)
+
+print(a.mean())
+
+print(a.prod())
+
+print(a.reshape([9,1]))
+
+print(a.sum())
+
+print(a.shape)
+
+
+
+print(a.transpose())
+
+print(a.trace())
+
+b = a
+print(a+b)
+
+print(a*b)
+
+print(a+2)
+
+print(a-3)
+
+print(a/2)
+
+print(np.matmul(a, b))
+np.average(a)
+print("*"*40)
+# open a file in numpy arr
+a = np.genfromtxt("data.txt", delimiter=",")
+print(a)
